449dc03295f0586cec92835fd62fd57ce4e7079f
aswin-code <aswinpops1234@gmail.com>
2022-10-15 21:50:29 +0530

    Initialize project using Create React App

---
+++ data.js
@@ -0,0 +1,32 @@
+export const books = [
+    {
+        title:,
+        author:,
+        image: "https://images-eu.ssl-images-amazon.com/images/I/81evNO+JD-L._AC._SR360,460.jpg",
+    },
+    {
+        title:,
+        author:,
+        image:,
+    },
+    {
+        title:,
+        author:,
+        image:,
+    },
+    {
+        title:,
+        author:,
+        image:,
+    },
+    {
+        title:,
+        author:,
+        image:,
+    },
+    {
+        title:,
+        author:,
+        image:,
+    },
+]
